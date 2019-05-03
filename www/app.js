62c1538414671ec41ca53595530327363b221623
paulchin <paulchin20@yahoo.com>
2019-05-03 15:40:05 +0800

    update

--- www/app.js
+++ www/app.js
@@ -1,15 +1,35 @@
+// document.addEventListener('init', function(event) {
+//   var page = event.target;
+//   console.log('test...');
+//   if (page === '1.html') {
+//     page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+//     page.querySelector('ons-toolbar .left').innerHTML =
+//       '<ons-back-button>Home</ons-back-button>';
+//   }
+// });
+
 window.fn = {};
 
-window.fn.open = function() {
-  //var menu = document.getElementById('menu');
-  var menu = $('#menu')[0];
+window.fn.openMenu = function() {
+  var menu = document.getElementById('menu');
   menu.open();
 };
 
 window.fn.load = function(page) {
-  //var content = document.getElementById('content');
-  var content = $('#content')[0];
-  //var menu = document.getElementById('menu');
-  var menu = $('#menu')[0];
+  var content = document.getElementById('content');
+
+  var menu = document.getElementById('menu');
+
   content.load(page).then(menu.close.bind(menu));
+};
+
+function goHome() {
+  fn.load('home.html');
+}
+
+window.fn.pushPage = function(page) {
+  // document
+  //   .getElementById('appNavigator')
+  //   .pushPage(page, { data: { title: page.title }, animation: 'slide' });
+  document.getElementById('appNavigator').pushPage(page);
 };
