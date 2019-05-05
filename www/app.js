5e09b180d25e18d175051cb50550b605be3ea3fd
paulchin <paulchin20@yahoo.com>
2019-05-05 17:33:55 +0800

    update

--- www/app.js
+++ www/app.js
@@ -14,21 +14,39 @@ window.fn.load = function(page, mytitle) {
 
 document.addEventListener('init', function(event) {
   var page = event.target;
-  //-- if - left here for future reference --
+  //-- scroll to top of home page --
   if (page.id === 'home.html') {
     page.querySelector('#backToTopBtn').onclick = function() {
-      document
-        .querySelector('#myNavigator')
-        .pushPage('home.html', {
-          data: { title: 'Page 1!' },
-          animation: 'lift'
-        });
+      document.querySelector('#myNavigator').pushPage('home.html', {
+        data: { title: 'Page 1!' },
+        animation: 'lift'
+      });
     };
-    //-- else if - left here for future reference --
-  } else if (page.id === '99.html' || page.id === '100.html') {
+  } else if (page.id === 'home.html') {
     page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+
+    //--- admob ---
+    showBannerAd();
+  } else if (page.id === '5.html' || page.id === '10.html') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+
+    //--- admob ---
+    showRewardedVideoAd();
   } else {
     //-- impt sets the page title --
     page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+
+    //--- admob ---
+    showInterstitialAd();
   }
 });
+
+//--- ADMOB---
+window.onload = function() {
+  document.addEventListener('deviceready', initApp);
+};
+
+function initApp() {
+  //--- Admob in ads.js ----
+  initAds();
+}
